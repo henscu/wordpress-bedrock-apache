9541673c4f8f6d98021770127af20cd90f0cdf77
Henry Scullion <henry_scullion@speakeazy.eu>
2017-07-15 17:29:06 +0200

    Tried to add wp-cli

--- php5.6/apache/docker-entrypoint.sh
+++ php5.6/apache/docker-entrypoint.sh
@@ -41,50 +41,54 @@ if [[ "$1" == apache2* ]] || [ "$1" == php-fpm ]; then
 		exit 1
 	fi
 
-	if ! [ -e index.php -a -e wp-includes/version.php ]; then
-		echo >&2 "WordPress not found in $(pwd) - copying now..."
-		if [ "$(ls -A)" ]; then
-			echo >&2 "WARNING: $(pwd) is not empty - press Ctrl+C now if this is an error!"
-			( set -x; ls -A; sleep 10 )
-		fi
-		tar cf - --one-file-system -C /usr/src/wordpress . | tar xf -
-		echo >&2 "Complete! WordPress has been successfully copied to $(pwd)"
-		if [ ! -e .htaccess ]; then
-			# NOTE: The "Indexes" option is disabled in the php:apache base image
-			cat > .htaccess <<-'EOF'
-				# BEGIN WordPress
-				<IfModule mod_rewrite.c>
-				RewriteEngine On
-				RewriteBase /
-				RewriteRule ^index\.php$ - [L]
-				RewriteCond %{REQUEST_FILENAME} !-f
-				RewriteCond %{REQUEST_FILENAME} !-d
-				RewriteRule . /index.php [L]
-				</IfModule>
-				# END WordPress
-			EOF
-			chown www-data:www-data .htaccess
-		fi
-	fi
+#	echo >&2 " MYSQL_ENV_MYSQL_ROOT_PASSWORD: $MYSQL_ENV_MYSQL_ROOT_PASSWORD MYSQL_ENV_MYSQL_PASSWORD: $MYSQL_ENV_MYSQL_PASSWORD MYSQL_ENV_MYSQL_DATABASE: $MYSQL_ENV_MYSQL_DATABASE"
 
-	# TODO handle WordPress upgrades magically in the same way, but only if wp-includes/version.php's $wp_version is less than /usr/src/wordpress/wp-includes/version.php's $wp_version
+	echo >&2 "WORDPRESS_DB_HOST: $WORDPRESS_DB_HOST WORDPRESS_DB_USER: $WORDPRESS_DB_USER WORDPRESS_DB_PASSWORD: $WORDPRESS_DB_PASSWORD WORDPRESS_DB_NAME: $WORDPRESS_DB_NAME"
 
-	# version 4.4.1 decided to switch to windows line endings, that breaks our seds and awks
-	# https://github.com/docker-library/wordpress/issues/116
-	# https://github.com/WordPress/WordPress/commit/1acedc542fba2482bab88ec70d4bea4b997a92e4
-	sed -ri -e 's/\r$//' wp-config*
+#	if ! [ -e index.php -a -e wp-includes/version.php ]; then
+#		echo >&2 "WordPress not found in $(pwd) - copying now..."
+#		if [ "$(ls -A)" ]; then
+#			echo >&2 "WARNING: $(pwd) is not empty - press Ctrl+C now if this is an error!"
+#			( set -x; ls -A; sleep 10 )
+#		fi
+#		tar cf - --one-file-system -C /usr/src/wordpress . | tar xf -
+#		echo >&2 "Complete! WordPress has been successfully copied to $(pwd)"
+#		if [ ! -e .htaccess ]; then
+#			# NOTE: The "Indexes" option is disabled in the php:apache base image
+#			cat > .htaccess <<-'EOF'
+#				# BEGIN WordPress
+#				<IfModule mod_rewrite.c>
+#				RewriteEngine On
+#				RewriteBase /
+#				RewriteRule ^index\.php$ - [L]
+#				RewriteCond %{REQUEST_FILENAME} !-f
+#				RewriteCond %{REQUEST_FILENAME} !-d
+#				RewriteRule . /index.php [L]
+#				</IfModule>
+#				# END WordPress
+#			EOF
+#			chown www-data:www-data .htaccess
+#		fi
+#	fi
+#
+#	# TODO handle WordPress upgrades magically in the same way, but only if wp-includes/version.php's $wp_version is less than /usr/src/wordpress/wp-includes/version.php's $wp_version
+#
+#	# version 4.4.1 decided to switch to windows line endings, that breaks our seds and awks
+#	# https://github.com/docker-library/wordpress/issues/116
+#	# https://github.com/WordPress/WordPress/commit/1acedc542fba2482bab88ec70d4bea4b997a92e4
+#	sed -ri -e 's/\r$//' wp-config*
 
-	if [ ! -e wp-config.php ]; then
-		awk '/^\/\*.*stop editing.*\*\/$/ && c == 0 { c = 1; system("cat") } { print }' wp-config-sample.php > wp-config.php <<'EOPHP'
-// If we're behind a proxy server and using HTTPS, we need to alert Wordpress of that fact
-// see also http://codex.wordpress.org/Administration_Over_SSL#Using_a_Reverse_Proxy
-if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
-	$_SERVER['HTTPS'] = 'on';
-}
+#	if [ ! -e wp-config.php ]; then
+#		awk '/^\/\*.*stop editing.*\*\/$/ && c == 0 { c = 1; system("cat") } { print }' wp-config-sample.php > wp-config.php <<'EOPHP'
+#// If we're behind a proxy server and using HTTPS, we need to alert Wordpress of that fact
+#// see also http://codex.wordpress.org/Administration_Over_SSL#Using_a_Reverse_Proxy
+#if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+#	$_SERVER['HTTPS'] = 'on';
+#}
 
-EOPHP
-		chown www-data:www-data wp-config.php
-	fi
+#EOPHP
+#		chown www-data:www-data wp-config.php
+#	fi
 
 	# see http://stackoverflow.com/a/2705678/433558
 	sed_escape_lhs() {
@@ -109,46 +113,46 @@ EOPHP
 		sed -ri -e "s/($start\s*).*($end)$/\1$(sed_escape_rhs "$(php_escape "$value" "$var_type")")\3/" wp-config.php
 	}
 
-	set_config 'DB_HOST' "$WORDPRESS_DB_HOST"
-	set_config 'DB_USER' "$WORDPRESS_DB_USER"
-	set_config 'DB_PASSWORD' "$WORDPRESS_DB_PASSWORD"
-	set_config 'DB_NAME' "$WORDPRESS_DB_NAME"
+#	set_config 'DB_HOST' "$WORDPRESS_DB_HOST"
+#	set_config 'DB_USER' "$WORDPRESS_DB_USER"
+#	set_config 'DB_PASSWORD' "$WORDPRESS_DB_PASSWORD"
+#	set_config 'DB_NAME' "$WORDPRESS_DB_NAME"
 
 	# allow any of these "Authentication Unique Keys and Salts." to be specified via
 	# environment variables with a "WORDPRESS_" prefix (ie, "WORDPRESS_AUTH_KEY")
-	UNIQUES=(
-		AUTH_KEY
-		SECURE_AUTH_KEY
-		LOGGED_IN_KEY
-		NONCE_KEY
-		AUTH_SALT
-		SECURE_AUTH_SALT
-		LOGGED_IN_SALT
-		NONCE_SALT
-	)
-	for unique in "${UNIQUES[@]}"; do
-		uniqVar="WORDPRESS_$unique"
-		file_env "$uniqVar"
-		if [ "${!uniqVar}" ]; then
-			set_config "$unique" "${!uniqVar}"
-		else
+#	UNIQUES=(
+#		AUTH_KEY
+#		SECURE_AUTH_KEY
+#		LOGGED_IN_KEY
+#		NONCE_KEY
+#		AUTH_SALT
+#		SECURE_AUTH_SALT
+#		LOGGED_IN_SALT
+#		NONCE_SALT
+#	)
+#	for unique in "${UNIQUES[@]}"; do
+#		uniqVar="WORDPRESS_$unique"
+#		file_env "$uniqVar"
+#		if [ "${!uniqVar}" ]; then
+#			set_config "$unique" "${!uniqVar}"
+#		else
 			# if not specified, let's generate a random value
-			current_set="$(sed -rn -e "s/define\((([\'\"])$unique\2\s*,\s*)(['\"])(.*)\3\);/\4/p" wp-config.php)"
-			if [ "$current_set" = 'put your unique phrase here' ]; then
-				set_config "$unique" "$(head -c1m /dev/urandom | sha1sum | cut -d' ' -f1)"
-			fi
-		fi
-	done
+#			current_set="$(sed -rn -e "s/define\((([\'\"])$unique\2\s*,\s*)(['\"])(.*)\3\);/\4/p" wp-config.php)"
+#			if [ "$current_set" = 'put your unique phrase here' ]; then
+#				set_config "$unique" "$(head -c1m /dev/urandom | sha1sum | cut -d' ' -f1)"
+#			fi
+#		fi
+#	done
 
-	file_env 'WORDPRESS_TABLE_PREFIX'
-	if [ "$WORDPRESS_TABLE_PREFIX" ]; then
-		set_config '$table_prefix' "$WORDPRESS_TABLE_PREFIX"
-	fi
+#	file_env 'WORDPRESS_TABLE_PREFIX'
+#	if [ "$WORDPRESS_TABLE_PREFIX" ]; then
+#		set_config '$table_prefix' "$WORDPRESS_TABLE_PREFIX"
+#	fi
 
-	file_env 'WORDPRESS_DEBUG'
-	if [ "$WORDPRESS_DEBUG" ]; then
-		set_config 'WP_DEBUG' 1 boolean
-	fi
+#	file_env 'WORDPRESS_DEBUG'
+#	if [ "$WORDPRESS_DEBUG" ]; then
+#		set_config 'WP_DEBUG' 1 boolean
+#	fi
 
 	TERM=dumb php -- "$WORDPRESS_DB_HOST" "$WORDPRESS_DB_USER" "$WORDPRESS_DB_PASSWORD" "$WORDPRESS_DB_NAME" <<'EOPHP'
 <?php
@@ -169,7 +173,8 @@ if (is_numeric($socket)) {
 
 $maxTries = 10;
 do {
-	$mysql = new mysqli($host, $argv[2], $argv[3], '', $port, $socket);
+	fwrite($stderr, "\n" . 'MySQL Connection: Host: '  . $host . ' User: ' . $argv[2] . ' Password: ' . $argv[3] . ' Database: ' . $argv[4] . ' Port: ' . $port . ' Socket: ' . $socket . "\n");
+	$mysql = new mysqli($host, $argv[2], $argv[3], $argv[4], $port, $socket);
 	if ($mysql->connect_error) {
 		fwrite($stderr, "\n" . 'MySQL Connection Error: (' . $mysql->connect_errno . ') ' . $mysql->connect_error . "\n");
 		--$maxTries;
@@ -177,6 +182,9 @@ do {
 			exit(1);
 		}
 		sleep(3);
+	}
+	else {
+		fwrite($stderr, "\n \n" . 'MySQL Connection Successful...' . $mysql->connect_errno . ') ' . $mysql->connect_error . "\n");
 	}
 } while ($mysql->connect_error);
 
